24749215e9257fb1726fcd20d6e269bd1aafc906
ros_mmerter <ros_mmerter@WROSRM31920.seattleschools.org>
2014-09-04 08:41:31 -0700

    First Commit

---
+++ src/Firstassignment.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Firstassignment {
+	public static void main (String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("Enter a number between 1-100");
+		System.out.println("or type \"Quit\" to exit");
+		String userInput = console.nextLine();
+		//if (userInput(int) <= 100 && <= 1){
+		//}else if(userInput.equals("quit"))
+	}
+
+	public int factor(){
+		return 1;
+	}
+}
