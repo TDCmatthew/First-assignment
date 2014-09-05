fe3e407ff22621ab5420f42b05d10c6b5cc97843
ros_mmerter <ros_mmerter@WROSRM31920.seattleschools.org>
2014-09-05 08:35:00 -0700

    Second commit.

--- src/Firstassignment.java
+++ src/Firstassignment.java
@@ -3,15 +3,30 @@ import java.util.Scanner;
 
 public class Firstassignment {
 	public static void main (String[] args) {
-		Scanner console = new Scanner(System.in);
-		System.out.println("Enter a number between 1-100");
-		System.out.println("or type \"Quit\" to exit");
-		String userInput = console.nextLine();
-		//if (userInput(int) <= 100 && <= 1){
-		//}else if(userInput.equals("quit"))
+
 	}
 
 	public int factor(){
 		return 1;
+	}
+	
+	public static int parseInput(){
+		Scanner console = new Scanner(System.in);
+		System.out.println("Enter a number between 1-100");
+		System.out.println("or type \"Quit\" to exit");
+		console.nextLine();
+		if(console.hasNextInt()){
+			int numberInput = console.nextLine();
+		}
+		if(userInput.equals("quit")){
+		}else{
+			System.out.println("You can't do that. Please enter a number between 1- 100, or type \"quit\"");
+			return 101;
+		}
+		
+		
+		
+		
+		return 0;
 	}
 }
